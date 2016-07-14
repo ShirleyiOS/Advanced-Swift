22798f414bd168b1fc8d92324af83103f9877d61
wangshuang <shuang.wang0909@gmail.com>
2016-07-14 15:27:18 +0800

    map filter reduce

--- CollectionType.playground/Contents.swift
+++ CollectionType.playground/Contents.swift
@@ -9,12 +9,12 @@ y.append(4)
 
 let a = NSMutableArray(array: [1,2,3])
 let b: NSArray = a
-a.insert(4, at: 3)
+a.insertObject(4, atIndex: 3)
 b
 
 let a1 = NSMutableArray(array: [1,2,3])
 let b1 = a1.copy()as! NSArray
-a1.insert(4, at: 3)
+a1.insertObject(4, atIndex: 3)
 b1
 
 // map
@@ -39,11 +39,85 @@ extension Array{
     }
 }
 
-extension Sequence{
-    func findElement(match: T->Bool) -> T? {
+extension SequenceType{
+    func findElement(match: Generator.Element->Bool) -> Generator.Element? {
         for element in self where match(element){
             return element
         }
         return nil
     }
 }
+
+extension Array{
+    func accumulate<U>(initial: U, combine: (U, Element) -> U) -> [U] {
+        var running = initial
+        return self.map{ next in
+            running = combine(running, next)
+            return running
+        }
+    }
+}
+
+[1,2,3,4].accumulate(0, combine: +)
+
+extension Array{
+    func filter(includeElement: Element -> Bool) -> [Element] {
+        var result: [Element] = []
+        for x in self where includeElement(x){
+            result.append(x)
+        }
+        return result
+    }
+}
+
+extension SequenceType{
+    public func allMatch(predicate: Generator.Element -> Bool) -> Bool{
+        return !self.contains{!predicate($0)}
+    }
+}
+
+
+fibs
+
+var total = 0
+for num in fibs{
+    total = total + num
+}
+total
+
+fibs.reduce(0){total, num in total + num}
+fibs.reduce(0, combine: +)
+
+fibs.reduce(""){str, num in str + "\(num)\n"}
+
+extension Array{
+    func reduce<U>(initial: U, combine: (U, Element) -> U) -> U {
+        var result = initial
+        for x in self{
+            result = combine(result, x)
+        }
+        return result
+    }
+}
+
+extension Array{
+    func map2<U>(transform: Element -> U) -> [U] {
+        return reduce([]){
+            $0 + [transform($1)]
+        }
+    }
+    
+    func filter2(includeElement: Element -> Bool) -> [Element] {
+        return reduce([]){
+            includeElement($1) ? $0 + [$1] : $0
+        }
+    }
+}
+
+let abc = [1,2,3].map2{fib in fib * fib}
+abc
+
+
+
+
+
